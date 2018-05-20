e5ddad78f64114f51af1eeea624a48856aca3916
Dor Sivandddג <dorsiv@gmail.comd>
2018-05-20 17:26:54 +0300

    commit

--- You Stupid Boy/You Stupid Boy/Controllers/HomeController.cs
+++ You Stupid Boy/You Stupid Boy/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace You_Stupid_Boy.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page lolz.";
 
             return View();
         }
